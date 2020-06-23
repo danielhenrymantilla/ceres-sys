4ee5f11b3b53e635736089f3ca16dfa0e5aefcd3
Daniel Henry-Mantilla <daniel.henry.mantilla@gmail.com>
2020-06-23 12:40:30 +0200

    Fix dangling pointer + other improvements
    These changes help improve the safety of the wrapper code

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,13 @@ mod ffi {
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
 
-use std::os::raw::{c_double, c_int, c_void};
+use ::std::{
+    core::convert::TryInto,
+    mem,
+    ptr::{self, null_mut as NULL},
+    slice,
+    os::raw::{c_double, c_int, c_void},
+};
 
 use ffi::{
     ceres_cost_function_t, ceres_create_problem, ceres_free_problem, ceres_init,
@@ -14,57 +20,83 @@ use ffi::{
 };
 
 #[repr(C)]
-struct ClosureData<'a> {
-    cost_fn: &'a mut dyn FnMut(&[f64], &mut [f64], Option<&mut [f64]>),
+struct ClosureData<F>
+where
+    F: FnMut(&[f64], &mut [f64], Option<&mut [f64]>)
+{
+    rust_closure: F,
     nparams: usize,
 }
+impl ClosureData {
+    fn as_mut_ptr(&mut self) -> *mut Self {
+        self
+    }
+}
 
-unsafe fn unpack_closures<F>(
-    mut closure: &mut F,
+fn unpack_closure<F>(
+    closure: F,
     nparams: usize,
-) -> (*mut c_void, ceres_cost_function_t)
+) -> (ClosureData<F>, ceres_cost_function_t)
 where
     F: FnMut(&[f64], &mut [f64], Option<&mut [f64]>),
 {
-    extern "C" fn trampoline(
+    #[allow(unused_unsafe)]
+    unsafe extern "C" fn trampoline<F>(
         data: *mut c_void,
         parameters: *mut *mut c_double,
-        residuals: *mut c_double,
-        jacobian: *mut *mut c_double,
-    ) -> c_int {
+        mut residuals: *mut c_double,
+        mut jacobian: *mut *mut c_double,
+    ) -> c_int
+    where
+        F: FnMut(&[f64], &mut [f64], Option<&mut [f64]>),
+    {
+        use ::core::{mem, slice};
+        let abort_on_drop_guard = {
+            struct AbortOnDrop;
+            impl Drop for AbortOnDrop { fn drop(&mut self) {
+                eprintln!("\
+                    Error, Rust was about to unwind across an `extern \"C\"` \
+                    function, which is Undefined Behavior.\n\
+                    Aborting the process for soundness.\
+                ");
+                ::std::process::abort();
+            }}
+            Guard
+        };          
         unsafe {
-            let closure_data: &mut ClosureData = &mut *(data as *mut ClosureData);
-            println!("{:p}", closure_data);
-            println!("{}", closure_data.nparams);
+            let mut data: *mut ClosureData = data.cast();
+            let closure_data: &mut ClosureData = data.as_mut().expect("Got NULL `data`");
             assert!(closure_data.nparams < 1e4 as usize);
-            let params = std::slice::from_raw_parts(parameters, 1);
-            let closure_params = std::slice::from_raw_parts(params[0], closure_data.nparams);
-            let mut closure_residuals =
-                std::slice::from_raw_parts_mut(residuals, closure_data.nparams);
-            let closure_jac = if jacobian.is_null() {
-                None
-            } else {
-                let jacobians = std::slice::from_raw_parts_mut(jacobian, 1);
-                if jacobians[0].is_null() {
-                    None
-                } else {
-                    Some(std::slice::from_raw_parts_mut(
-                        jacobians[0],
-                        closure_data.nparams,
-                    ))
+            let slice = |ptr: *const c_double| unsafe {
+                if ptr.is_null() {
+                    panic!("Got NULL slice pointer");
                 }
+                slice::from_raw_parts(ptr, closure_data.nparams)
             };
-            (*closure_data.cost_fn)(&closure_params, &mut closure_residuals, closure_jac);
+            let slice_mut = |ptr: *mut c_double| unsafe {
+                if ptr.is_null() {
+                    panic!("Got NULL slice pointer");
+                }
+                slice::from_raw_parts_mut(ptr, closure_data.nparams)
+            };
+            let &params: &(*mut c_double) = parameters.as_ref().expect("Got NULL `parameters`");
+            let params: &[c_double] = slice(params);
+            let &mut residuals: &mut (*mut c_double) = residuals.as_mut().expect("Got NULL `residuals`");
+            let closure_residuals: &mut [c_double] = slice_mut(residuals);
+            let closure_jac = jacobian.as_mut().map(|&mut ptr: &mut (*mut c_double)| {
+                slice_mut(ptr)
+            });
+            (closure_data.rust_closure)(closure_params, closure_residuals, closure_jac);
         }
+        // If we reach this point, no panic has happened, so we can defuse the abort bomb
+        mem::forget(abort_on_drop_guard);
         1
     }
-    let data = &mut ClosureData {
-        cost_fn: &mut closure,
+    let data = ClosureData {
+        rust_closure: closure,
         nparams,
-    } as *mut ClosureData as *mut c_void;
-    println!("{:p}", data);
-    println!("{}", nparams);
-    (data, Some(trampoline))
+    };
+    (data, Some(trampoline::<F>))
 }
 
 pub struct CeresSolver {
@@ -72,34 +104,50 @@ pub struct CeresSolver {
 }
 
 impl CeresSolver {
+    /// Docs yadda yadda
+    ///
+    /// # Safety
+    ///
+    ///   - Can only be called once.
+    #[allow(unused_unsafe)]
     pub unsafe fn new() -> Self {
-        ceres_init();
+        unsafe {
+            // Safety: guaranteed by the caller /* Or the Once guard */
+            ffi::ceres_init();
+        }
         Self {
-            problem: ceres_create_problem(),
+            problem: unsafe {
+                // Safety: FFI Wrapper + `ceres_init()` has been called.
+                ffi::ceres_create_problem()
+            },
         }
     }
 
-    pub fn solve<R>(&self, residual_function: &mut R, x0: &mut [f64])
-    where
-        R: FnMut(&[f64], &mut [f64], Option<&mut [f64]>),
-    {
-        let (data, resid_func) = unsafe { unpack_closures(residual_function, x0.len()) };
+    pub fn solve<R>(
+        &mut self,
+        residual_function: impl FnMut(&[f64], &mut [f64], Option<&mut [f64]>),
+        x0: &mut [f64],
+    ) {
+        let (mut data, resid_func) = unpack_closures(residual_function, x0.len());
         let mut x_ptr = x0.as_mut_ptr();
+        let mut len: i32 = x0.len().try_into()
         unsafe {
-            ceres_problem_add_residual_block(
+            // Safety: ...
+            ffi::ceres_problem_add_residual_block(
                 self.problem,
                 resid_func,
-                data,
+                data.as_mut_ptr().cast(),
                 None,
-                std::ptr::null_mut(),
-                x0.len() as i32,
+                NULL(),
+                len,
                 1,
-                &mut x0.len() as *mut usize as *mut i32,
-                &mut x_ptr as *mut *mut f64,
+                &mut len,
+                &mut x_ptr,
             );
         }
         unsafe {
-            ceres_solve(self.problem);
+            // Safety: ...
+            ffi::ceres_solve(self.problem);
         }
     }
 }
@@ -107,7 +155,8 @@ impl CeresSolver {
 impl Drop for CeresSolver {
     fn drop(&mut self) {
         unsafe {
-            ceres_free_problem(self.problem);
+            // Safety: FFI Wrapper + `self.problem` originates from `ceres_crate_problem`
+            ffi::ceres_free_problem(self.problem);
         }
     }
 }
